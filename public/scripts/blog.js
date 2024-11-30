30b07e56a79b0dff193f673a936ffbc506527f43
Abdellah <144657339+ActualLearner@users.noreply.github.com>
2024-11-30 19:27:27 +0300

    fix: correct url paths

--- public/scripts/blog.js
+++ public/scripts/blog.js
@@ -3,28 +3,26 @@ let deleteBtn = document.querySelector(".delete");
 let id = document.querySelector(".content").dataset.id;
 
 deleteBtn.addEventListener("click", async () => {
+  
+  let url = `/${id}`;
 
-  await fetch(`http://localhost:3000/${id}`, {
-    method: "DELETE"
-  })
-    .then(response => {
-      if (response.status === 204) {
-        console.log("Blog deleted successfully.");
-        alert("Blog deleted successfully!");
-        window.location.href = "/"; // Redirect to home page after deletion
-      } else {
-        return response.json(); // Parse the error message from response
-      }
-    })
-    .then(data => {
-      if (data) {
-        // If there's an error message, log it
-        console.error("Error:", data.message);
-        alert("Failed to delete blog.");
-      }
-    })
-    .catch(error => {
-      console.error("Error:", error);  // Handle other errors
-      alert("Failed to delete blog.");
+  try {
+    const response = await fetch(url, {
+      method: "DELETE"
     });
-})
+
+    if (response.status === 204) {
+      console.log("Blog deleted successfully.");
+      alert("Blog deleted successfully!");
+      window.location.href = "/";
+    } else {
+      const data = await response.json();
+      console.error("Error:", data.message);
+      alert("Failed to delete blog.");
+    }
+
+  } catch (error) {
+    console.error("Error:", error);  // Handle other errors
+    alert("Failed to delete blog.");
+  }
+});
