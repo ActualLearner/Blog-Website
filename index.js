5c534557a9d6dcaa2a243c5bcdb5c5fd8a24550e
Abdellah <144657339+ActualLearner@users.noreply.github.com>
2024-11-30 19:23:21 +0300

    fix: fix dir paths

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from "express";
-import ejs from "ejs";
 import path from "path";
+import ejs from "ejs";
 import { fileURLToPath } from "url";
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url); // Get the current file's URL
-const __dirname = path.dirname(__filename); // Get the current directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 let blogs = [];
 let currentID = 1;
@@ -15,15 +15,15 @@ function getID() {
 }
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // Explicitly set the views directory
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("./index.ejs", { blogs: blogs });
-})
+    res.render("index", { blogs: blogs });
+});
 
 app.get("/:id", (req, res) => {
-
     const id = req.params.id;
     const blog = blogs.find(blog => blog.id == id);
 
@@ -31,44 +31,37 @@ app.get("/:id", (req, res) => {
         return res.status(404).send("Blog not found");
     }
 
-    res.render("./blog.ejs", { blog });
-
+    res.render("blog", { blog });
 });
 
 app.post("/", (req, res) => {
-
     let blog = { title: req.body.title, content: req.body.content, id: getID() };
 
     if (blog.title == "" || blog.content.trim() == "") {
         res.status(400).send("Blog is empty");
     } else {
         blogs.push(blog);
-        res.render("./index.ejs", { blogs });
+        res.render("index", { blogs });
     }
-
-})
+});
 
 app.delete("/:id", (req, res) => {
     const id = req.params.id;
-    console.log(id);
 
-    let filteredBlogs = blogs.filter(elem => {
-        return elem.id != id;
-    })
+    let filteredBlogs = blogs.filter(elem => elem.id != id);
 
     if (blogs.length != filteredBlogs.length) {
-
         blogs = filteredBlogs;
         blogs.forEach(elem => {
             elem.id = getID();
         });
         res.status(204).send({ message: "Blog deleted successfully" });
-
     } else {
         res.status(404).send({ message: "Blog not found" });
     }
-})
+});
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening on port ${process.env.PORT}`);
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
